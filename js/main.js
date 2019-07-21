34ec9827d9b0002a0f660121a7b3a49d0fd4f3ba
rednaxela700 <ohleg@windowslive.com>
2019-07-21 21:33:23 +0200

    added buttons and a counter

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by alexander on 21/07/2019.
  */
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImg = document.querySelectorAll('.carousel-slide img');
+
+//buttons
+const prevBtn = document.querySelector('#carouselPrevBtn');
+const nextBtn = document.querySelector('#carouselNextBtn');
+
+//counter
+let counter = 1;
+const size = carouselImg[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
+
+nextBtn.addEventListener('click', ()=>{
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+});
