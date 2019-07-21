aa64631e1f310393dca37ba0e547deb519c86549
rednaxela700 <ohleg@windowslive.com>
2019-07-22 01:02:22 +0200

    added onclick and transitionend events, MVP

--- js/main.js
+++ js/main.js
@@ -15,5 +15,26 @@ const size = carouselImg[0].clientWidth;
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
 
 nextBtn.addEventListener('click', ()=>{
+    if (counter <= 0) {return}
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
+});
+
+prevBtn.addEventListener('click', ()=>{
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px';
+});
+
+carouselSlide.addEventListener('transitionend', ()=> {
+    console.log("Fired!");
+    if (carouselImg[counter].id === "lastClone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImg.length - 2;
+    } else if (carouselImg[counter].id ==="firstClone") {
+        carouselSlide.style.transition = "none";
+        counter = carouselImg.length - counter;
+    }
+
 });
